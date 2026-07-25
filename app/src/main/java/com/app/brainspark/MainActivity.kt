1767003b7001f74b592b67b0b121c230cd394311
sefes3000-dev <sefes3000@gmail.com>
2026-07-25 19:07:58 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/app/brainspark/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.app.brainspark
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.app.brainspark.ui.HomeScreen
+import com.app.brainspark.ui.theme.BrainSparkTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BrainSparkTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
